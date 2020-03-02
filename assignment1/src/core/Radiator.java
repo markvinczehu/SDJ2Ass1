b24fa248968b15c3bc9b219ac088997e2060f4bc
markvincze <mark.vincze.hu@gmail.com>
2020-03-02 12:00:58 +0100

    test

--- assignment1/src/core/Radiator.java
+++ assignment1/src/core/Radiator.java
@@ -2,14 +2,6 @@ package core;
 
 public class Radiator {
     public static void main(String[] args) {
-        System.out.println("lol");
-        //suck a dick
-        //Taking mad pills. My homie. You know me. Slicin off the tip. Going for a trip. Lightin up a spliff
-        //fuck youuuuuuuu
-        //Dont tell me what to do
-        if(0!=x) {
-            () -> suckMyDick;
-        }
-        System.out.println("I slowly begin to remove your shirt. Your nipple hard enough to cut diamond. I undo my trousers. My lizard is peeking from within");
+        seriousCode.get;
     }
 }
