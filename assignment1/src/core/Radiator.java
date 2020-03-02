3b07696ef0e69d59f1b6f220ec2ac7350543ec2f
Jimant2 <jimantoniou2@gmail.com>
2020-03-02 11:53:53 +0100

    Spinn

--- assignment1/src/core/Radiator.java
+++ assignment1/src/core/Radiator.java
@@ -3,5 +3,7 @@ package core;
 public class Radiator {
     public static void main(String[] args) {
         System.out.println("lol");
+        //suck a dick
+        //Taking mad pills. My homie. You know me. Slicin off the tip. Going for a trip. Lightin up a spliff
     }
 }
