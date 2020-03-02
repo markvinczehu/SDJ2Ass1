269512472cba260fabadd2b1d989183c7f97e1f5
markvincze <mark.vincze.hu@gmail.com>
2020-03-02 11:56:01 +0100

    test

--- assignment1/src/core/Radiator.java
+++ assignment1/src/core/Radiator.java
@@ -5,5 +5,6 @@ public class Radiator {
         System.out.println("lol");
         //suck a dick
         //Taking mad pills. My homie. You know me. Slicin off the tip. Going for a trip. Lightin up a spliff
+        //fuck youuuuuuuu
     }
 }
