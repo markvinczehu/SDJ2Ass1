181e7a26312d5ad031e0ada155c5f4c88360c9cb
markvinczehu <55513132+markvinczehu@users.noreply.github.com>
2020-03-03 11:51:40 +0100

    Running test

--- assignment1/src/RunTemperatureApp.java
+++ assignment1/src/RunTemperatureApp.java
@@ -1,3 +1,8 @@
-public class RunTemperatureApp
-{
+import javafx.application.Application;
+
+public class RunTemperatureApp {
+
+    public static void main(String[] args) {
+        Application.launch(TemperatureApp.class);
+    }
 }
