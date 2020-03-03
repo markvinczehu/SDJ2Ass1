c994f31a24cd99650ca43e95a1db981d7f50ac55
Hacklberry <matej.hackl@hotmail.com>
2020-03-03 11:37:45 +0100

    03/03/2020 Matej Hackl

--- assignment1/src/core/ViewModelFactory.java
+++ assignment1/src/core/ViewModelFactory.java
@@ -1,6 +1,23 @@
 package core;
 
+import viewmodel.BarChartViewModel;
+
 public class ViewModelFactory
 {
+  private ModelFactory modelFactory;
+  private BarChartViewModel barChartViewModel;
 
+  public ViewModelFactory(ModelFactory modelFactory)
+  {
+    this.modelFactory = modelFactory;
+  }
+
+  public BarChartViewModel getBarChartViewModel()
+  {
+    if(barChartViewModel == null)
+    {
+    barChartViewModel = new BarChartViewModel(modelFactory.getRadiatorState());
+    }
+    return barChartViewModel;
+  }
 }
