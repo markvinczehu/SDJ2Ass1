cc9210c2d3703e150ba70842d02b42c1e254a25a
markvincze <mark.vincze.hu@gmail.com>
2020-03-04 10:00:50 +0100

    rewrote ViewModelFactory

--- assignment1/src/core/ViewModelFactory.java
+++ assignment1/src/core/ViewModelFactory.java
@@ -3,26 +3,27 @@ package core;
 import viewmodel.BarChartViewModel;
 import viewmodel.LineChartViewModel;
 
-public class ViewModelFactory
-{
+public class ViewModelFactory {
+
   private ModelFactory modelFactory;
   private BarChartViewModel barChartViewModel;
+  private LineChartViewModel lineChartViewModel;
 
-  public ViewModelFactory(ModelFactory modelFactory)
-  {
+  public ViewModelFactory(ModelFactory modelFactory) {
     this.modelFactory = modelFactory;
   }
 
   public BarChartViewModel getBarChartViewModel() {
-    if(barChartViewModel == null)
-    {
+    if(barChartViewModel == null) {
     barChartViewModel = new BarChartViewModel(modelFactory.getRadiatorState());
     }
     return barChartViewModel;
   }
 
   public LineChartViewModel getLineChartViewModel() {
-
-
+    if(getLineChartViewModel() == null) {
+      lineChartViewModel = new LineChartViewModel(modelFactory.getRadiatorState());
+    }
+    return lineChartViewModel;
   }
 }
