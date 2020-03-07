818b7a0d24ebf15377ad5a282bc6d38bad5982bf
Jimant2 <jimantoniou2@gmail.com>
2020-03-07 23:17:13 +0100

    SpEEEEEEEEEEEEEEEEnn

--- assignment1/src/core/ViewHandler.java
+++ assignment1/src/core/ViewHandler.java
@@ -20,27 +20,29 @@ public class ViewHandler {
   }
 
   public void start() throws Exception {
-    //openView("BarChart");
     openView("LineChart");
+    openView("Bar Chart");
   }
 
   public void openView(String viewToOpen) throws IOException {
     Scene scene;
     FXMLLoader loader = new FXMLLoader();
     Parent root = null;
-    if("LineChart".equals(viewToOpen)) {
+    if("LineChart".equals(viewToOpen))
+    {
       loader.setLocation(getClass().getResource("../view/LineChartView.fxml"));
       root = loader.load();
       LineChartViewController view = loader.getController();
       view.init(mvViewModel.getLineChartViewModel());
       stage.setTitle("Line Chart");
-    } /*else if("BarChart".equals(viewToOpen)) {
+    }
+    else if("BarChart".equals(viewToOpen)) {
       loader.setLocation(getClass().getResource("../view/BarChartView.fxml"));
       root = loader.load();
       BarChartViewController view = loader.getController();
       view.init(mvViewModel.getBarChartViewModel());
       stage.setTitle("Bar Chart");
-    }*/
+    }
 
     scene = new Scene(root);
     stage.setScene(scene);
