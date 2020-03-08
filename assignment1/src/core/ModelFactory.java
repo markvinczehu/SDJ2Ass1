771ba36ca87c71129e3880ad0cded775b77b56f5
Jimant2 <jimantoniou2@gmail.com>
2020-03-08 15:56:37 +0100

    SpEEEEEEEEEEEEEEEEnn

--- assignment1/src/core/ModelFactory.java
+++ assignment1/src/core/ModelFactory.java
@@ -1,17 +1,19 @@
 package core;
 
 import mediator.TemperatureModel;
+import mediator.TemperatureModelManager;
 import model.Radiator;
 import model.RadiatorState;
+import model.Temperature;
 import view.BarChartViewModel;
 import view.LineChartViewModel;
 
 public class ModelFactory
 {
-  private Model model;
+  private TemperatureModel model;
 
-  public Model getModel() {
-    if(model == null) model = new DataModel();
+  public TemperatureModel getModel() {
+    if(model == null) model = new TemperatureModelManager();
     return model;
   }
 }
